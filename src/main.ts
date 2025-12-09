72c5a305bca2701c0ea82f2c8f79c8f96dfb2913
matisantillan11 <matias1.santillan@gmail.com>
2025-12-09 00:30:11 -0300

    chore: Update default application listening port from 3001 to 8080.

--- src/main.ts
+++ src/main.ts
@@ -47,7 +47,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 8080);
   console.log(
     `🚀 Application is running on: http://localhost:${process.env.PORT ?? 3001}`,
   );
